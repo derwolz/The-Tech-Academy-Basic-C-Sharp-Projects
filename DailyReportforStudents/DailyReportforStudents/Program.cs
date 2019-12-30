c06b0611fe3a2977b0ac822f093b7297c5e11743
Blaine Wolz <der.wolz@gmail.com>
2019-12-30 16:00:46 -0700

    add daily report project

---
+++ DailyReportforStudents/DailyReportforStudents/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DailyReportforStudents
+{
+    class Program
+    {
+        static void Main() 
+        {
+            
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What Course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What Page are you on?");
+            int currentPage = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? (true/false)");
+            bool parseHelp = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please be specific.");
+            string positiveExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An instructor will contact you shortly. Have a great day!");
+            Console.Read();
+            
+               
+
+        }
+    }
+}
--- Welcome Screen Accounting/Welcome Screen Accounting/Program.cs
+++ Welcome Screen Accounting/Welcome Screen Accounting/Program.cs
@@ -6,8 +6,17 @@ namespace Welcome_Screen_Accounting
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Acme Accounting Systems");
-            Console.WriteLine("Remember, we're  \"accounting\" on you!");
+            //Console.WriteLine("Welcome to Acme Accounting Systems");
+            //Console.WriteLine("Remember, we're  \"accounting\" on you!");
+            //Console.WriteLine("What is your farorite Number?");
+            //int favoriteNumber = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine($"Your number plus 5 is: {favoriteNumber + 5}");
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char randomLetter = 'K';
+            char questionMark = '\u2103';
+            Console.WriteLine(questionMark);
         }
     }
 }
