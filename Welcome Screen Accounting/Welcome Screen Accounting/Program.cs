f6e187d0730b584681397c16d37fa59283863016
Blaine Wolz <der.wolz@gmail.com>
2019-12-30 15:21:04 -0700

    initial push accounting screen

---
+++ Welcome Screen Accounting/Welcome Screen Accounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Welcome_Screen_Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're  \"accounting\" on you!");
+        }
+    }
+}
