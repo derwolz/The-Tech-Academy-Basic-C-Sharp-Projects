4717f80fe1966d61e2470c8a32d7b0dd895c0ca3
Blaine Wolz <der.wolz@gmail.com>
2019-12-30 21:33:15 -0700

    add package quote calculator

---
+++ Package quote program/Package quote program/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Package_quote_program
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            int maxweight = 50;
+            int maxDimensions = 50;
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("What is the weight of the package?");
+            int packageWeight = Int32.Parse(Console.ReadLine());
+            if (packageWeight > maxweight)
+            {
+                Console.WriteLine("Package is too heavy to ship, Have a good day");
+                Console.Read();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("What is the width of the package?");
+            int packageWidth = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What is the height of the package?");
+            int packageHeight = Int32.Parse(Console.ReadLine());
+            if (packageHeight + packageWidth > maxDimensions)
+            {
+                Console.WriteLine("That package is too large to ship. Have a good day.");
+                Console.Read();
+                Environment.Exit(0);
+            }
+            float quote = (packageWidth + packageHeight) * packageWeight;
+            quote = quote / 100;
+            Console.WriteLine($"Your package will cost ${quote} to ship.");
+            Console.Read();
+            
+         
+        }
+    }
+}
