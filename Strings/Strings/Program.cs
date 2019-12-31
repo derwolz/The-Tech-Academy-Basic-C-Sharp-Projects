3d28c5b5067e608cf0f3349910b31ac11b40df15
Blaine Wolz <der.wolz@gmail.com>
2019-12-31 12:32:24 -0700

    Add string program but for real this time

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "Hello ";
+            string word2 = "David";
+            string word3 = "!";
+            Console.WriteLine(word1 + word2 + word3);
+            string word4 = "all of these are lower case.";
+            Console.WriteLine(word4);
+            Console.WriteLine("the last set will now be upper case");
+            Console.WriteLine(word4.ToUpper());
+            StringBuilder thisparagraph = new StringBuilder();
+            thisparagraph.Append("This is the 1st sentence of the paragraph.");
+            Console.WriteLine(thisparagraph);
+            thisparagraph.Append("\n Now I have added a second line to the paragraph");
+            Console.WriteLine(thisparagraph);
+            thisparagraph.Append("\n Doesn't this work well?");
+            Console.WriteLine(thisparagraph);
+            Console.Read();
+        }
+    }
+}
