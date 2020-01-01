a73b4a79ddd285da7a2e4f26aa825435e0df3b2f
Blaine Wolz <der.wolz@gmail.com>
2020-01-01 15:11:26 -0700

    add out methods in program

--- Blackjack/Blackjack/Program.cs
+++ Blackjack/Blackjack/Program.cs
@@ -7,43 +7,43 @@ namespace Blackjack
     {
         static void Main(string[] args)
         {
+            int x = 7;
+            int y = 19;
+            two_outs Two = new two_outs();
+         
+            Console.WriteLine("X:{0} Y:{1}", x, y);
+            Two.Changebothtofive(out x,out y);
+            Console.WriteLine("X:{0} Y:{1}", x, y);
             
-            NumberManipulator num1 = new NumberManipulator();
-            Console.WriteLine("Give me a number");
-            try 
-            { 
-                int numquery = Int32.Parse(Console.ReadLine());
-                Console.WriteLine($"{numquery} added to 7 is {num1.add7(numquery)}");
-                Console.WriteLine($"10 divided by {numquery} is {num1.divTenBy(numquery)}");
-                Console.WriteLine($"Is {numquery} an even number? {num1.isEven(numquery)}");
-
-            }
-            catch (FormatException) { Console.WriteLine("that is not a number"); }
-            catch (DivideByZeroException) { Console.WriteLine("Cannot divide by zero"); }
+            
             Console.ReadLine();
             Console.Clear();
             Console.WriteLine("Here is a randomly sorted deck of standard playing cards");
 
             Deck deck = new Deck();
             Console.WriteLine(deck.Cards.Count);
-            deck = Shuffle(deck);
+            deck = Shuffle(deck, 5);
             foreach ( Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
         }
-        public static Deck Shuffle(Deck deck)
+        public static Deck Shuffle(Deck deck, int times = 1)
         {
-            List<Card> TempList = new List<Card>();
-            Random rand = new Random();
-            while (deck.Cards.Count > 0)
-            {
-                int randomIndex = rand.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
+            for (int i = 0; i < times; i++)
+            { 
+                    List<Card> TempList = new List<Card>();
+                Random rand = new Random();
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = rand.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
             }
-            deck.Cards = TempList;
             return deck;
         }
+        
     }
 }
