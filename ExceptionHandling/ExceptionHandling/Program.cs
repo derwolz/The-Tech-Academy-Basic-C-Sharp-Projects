1875682541af96e2ac22746ce75960605a4dbf87
Blaine Wolz <der.wolz@gmail.com>
2020-01-01 11:22:52 -0700

    add Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Random rand = new Random();
+                int listlength = rand.Next(1, 10);
+                List<int> intlist = new List<int>();
+                for (int i = 0; i < listlength; i++)
+                {
+                    intlist.Add(rand.Next(1, 100));
+                }
+                Console.WriteLine("Choose a number to divide by");
+                int numquery = Int32.Parse(Console.ReadLine());
+                for (int i = 0; i < listlength; i++)
+                {
+                    int oldnum = intlist[i];
+                    intlist[i] = intlist[i] / numquery;
+                    Console.WriteLine($"{oldnum} divided by {numquery} to the closest whole number equals {intlist[i]}");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("That is not a number.");
+                string databaselog = ex.Message;
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                string databaselog = ex.Message;
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
