69e72699fb33b07471f8b31f8fb789305b2b33e0
Blaine Wolz <der.wolz@gmail.com>
2020-01-03 02:15:43 -0700

    add 3math method app

---
+++ 3mathapp/3mathapp/Mather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3mathapp
+{
+    static class Mather
+    {
+        static public int addfive(int x)
+        {
+            return x + 5;
+        }
+        static public int MultiplybyFive(int x)
+        {
+            return x * 5;
+        }
+        static public int SubtractFive(int x)
+        {
+            return x - 5;
+        }
+    }
+}
---
+++ 3mathapp/3mathapp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3mathapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Give me a number");
+                int num = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("{0} plus 5 = {1}", num, Mather.addfive(num));
+                Console.WriteLine("{0} times 5 = {1}", num, Mather.MultiplybyFive(num));
+                Console.WriteLine("{0} minus 5 = {1}", num, Mather.SubtractFive(num));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That is not a number");
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
