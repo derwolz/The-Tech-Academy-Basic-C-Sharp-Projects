0fbbb96e01f8bce904cf507357eedfa6eeb3cf42
Blaine Wolz <der.wolz@gmail.com>
2020-01-02 17:01:38 -0700

    add overload document

---
+++ Return Int/Return Int/Num returner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Return_Int
+{
+    class Num_returner
+    {
+        public int addfive(int x)
+        {
+            return x += 5;
+        }
+        public int addfive(decimal x)
+        {
+                int y = Convert.ToInt32(x * 5);
+                return y;
+        }
+        public int addfive(string x)
+        {
+            try
+            {
+                int y = Convert.ToInt32(x);
+                y -= 3;
+                return y;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Cannot convert to int.");
+                return -1;
+            }
+            
+        }
+    }
+}
---
+++ Return Int/Return Int/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Return_Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Num_returner newnum = new Num_returner();
+            Console.WriteLine("12 + 5 = {0}.\nMultiplying decimal 12.3 by 5: {1}.\nConverting string 12 to integer then subtracting 3: {2}",
+                newnum.addfive(12), newnum.addfive(12.7m), newnum.addfive("12"));
+        }
+    }
+}
