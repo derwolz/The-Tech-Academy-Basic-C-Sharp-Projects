8f607e50cc25f31cd9606f0d104c328eb32d355d
Blaine Wolz <der.wolz@gmail.com>
2020-01-03 02:39:36 -0700

    add two parameter method

---
+++ Two parameter method/Two parameter method/Number Manipulator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Two_parameter_method
+{
+    class NumberManipulator
+    {
+        public int add7(int x)
+        {
+            x = x + 7;
+            return x;
+        }
+        public int divTenBy(int x)
+        {
+            x = x / 10;
+            return x;
+        }
+        public bool isEven(int x)
+        {
+            if (x % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+        public void Changebothtofive(int x, out int f)
+        {
+
+            f = x+2;
+            return;
+
+        }
+    }
+}
---
+++ Two parameter method/Two parameter method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Two_parameter_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            int y = 19;
+            NumberManipulator Two = new NumberManipulator();
+
+            Console.WriteLine("X:{0} Y:{1}", x, y);
+            Two.Changebothtofive(x:x, f: out y);
+            Console.WriteLine("X:{0} Y:{1}", x, y);
+            Console.ReadLine();
+        }
+    }
+}
