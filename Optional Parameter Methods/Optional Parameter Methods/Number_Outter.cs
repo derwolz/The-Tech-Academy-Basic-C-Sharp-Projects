ce7f9a8b891e24b9538fb6deb0110097891fba8e
Blaine Wolz <der.wolz@gmail.com>
2020-01-02 17:17:08 -0700

    add optional parameter method

---
+++ Optional Parameter Methods/Optional Parameter Methods/Number_Outter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Optional_Parameter_Methods
+{
+    class Number_Outter
+    {
+        public int TimesMyNumbers(int x, int y = 0)
+        {
+            int result = x * y;
+            return result;
+        }
+    }
+}
---
+++ Optional Parameter Methods/Optional Parameter Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Optional_Parameter_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number");
+                Number_Outter newNumber = new Number_Outter();
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input a second number (or hit Enter to skip)");
+            string num2String = Console.ReadLine();
+            if (num2String == "")
+                Console.WriteLine("{0} times nothing equals {1}", num1, newNumber.TimesMyNumbers(num1));
+            else
+                try
+                {
+                    int num2 = Convert.ToInt32(num2String);
+                    Console.WriteLine("{0} times {1} equals {2}", num1, num2, newNumber.TimesMyNumbers(num1, num2));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not a number");
+                }
+        }
+    }
+}
