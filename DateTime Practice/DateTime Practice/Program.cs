88f4ccf904ecaf307b42267ba0a4961eedca3ffa
Blaine Wolz <der.wolz@gmail.com>
2020-01-05 17:59:44 -0700

    add datetime practice

---
+++ DateTime Practice/DateTime Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DateTime_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Give me a number");
+            int numQuery = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("It will be {0}", DateTime.Now.AddHours(numQuery));
+            Console.Read();
+        }
+    }
+}
